e8d369682470a22183f50b3c5ffb4d3159ed1162
nitro583 <seiyamatsumoto@me.com>
2021-02-17 00:01:38 +0900

    todosの削除機能実装

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="page">
+    <h1>Todo list</h1>
     <ul>
-      <li v-for="todo in todos" v-bind:key="todo.id" v-on:click=>
+      <li
+        v-for="(todo,index) in todos"
+        v-bind:key="todo.id"
+
+      >
         {{ todo }}
+        <button v-on:click="deleteTodo(index)">Delete</button>
       </li>
     </ul>
     <div class="inputform">
       <form v-on:submit.prevent="submitTodo">
-        <input v-model="todo"
-        type="text"
-        placeholder="Add a Todo"
-        >
-        <button tyoe="submit">
-          Add Todo
-        </button>
+        <input v-model="todo" type="text" placeholder="Add a Todo" />
+        <button tyoe="submit">Add Todo</button>
       </form>
     </div>
   </div>
@@ -23,19 +24,22 @@ export default {
   data() {
     return {
       todos: ["これと", "それと", "あれと"],
-      todo:"",
+      todo: "",
     };
   },
   methods: {
     submitTodo() {
-      if(this.todo) {
-        this.todos.push(this.todo)
-        this.todo = ''
+      if (this.todo) {
+        this.todos.push(this.todo);
+        this.todo = "";
       }
-    }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+      console.log(index);
+    },
   },
 };
 </script>
 <style>
-
 </style>
