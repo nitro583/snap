58e8eb9098d18f4ea13afd1d97e02f72cf9326ff
nitro583 <seiyamatsumoto@me.com>
2021-03-10 00:01:48 +0900

    スペースの削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,6 @@
       </v-navigation-drawer>
 
       <v-app-bar app>
-
         
         <div><NuxtLink to="/">index</NuxtLink></div>
         <div><NuxtLink to="upload">Upload</NuxtLink></div>
@@ -14,6 +13,7 @@
         <div><NuxtLink to="register">Signup</NuxtLink></div>
         <div><NuxtLink to="login">Login</NuxtLink></div>
         <NuxtLink to="todos">Todos</NuxtLink>
+
         <logOut></logOut> </v-app-bar
       ><!-- アプリケーションのコンポーネントに基づいてコンテンツのサイズを決定 --><v-main
         ><!-- アプリケーションに適切なgutterを提供します -->
